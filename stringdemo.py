5eddba6f96d8f112d0f78413b2d8866cd6b9bb18
Harsh Sisodiya <harshsisodiya678@Harshs-MacBook-Pro.local>
2026-03-03 13:58:57 +0800

    2-3-26

---
+++ stringdemo.py
@@ -0,0 +1,13 @@
+s = "Tops Technologies"
+
+print(s)
+print(s.capitalize())
+print(s.casefold())
+print(s.center(50,"-"))
+print(s.lower())
+print(s.find("log"))
+print(s.index("T",2))
+print("tops123".isalnum())
+print(s.istitle())
+print(" " .isspace())
+print("tops".islower())
---
+++ stringslice.py
@@ -0,0 +1,14 @@
+s = "Tops Technologies"
+
+print(s[3:15])
+print(s[:13])
+print(s[2:])
+print(s[2:14:3])
+print(s[::5])
+
+
+print(s[-15:-3])
+print(s[:-2])
+print(s[-14:])
+print(s[-16:-1:4])
+print(s[::-1])
